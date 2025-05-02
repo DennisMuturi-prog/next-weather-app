f469dc3f3081878cde7129b55008f449de1f9b6e
Dennis Muturi <deynohmuturi@gmail.com>
2025-05-02 21:05:54 +0300

    feat: enhance Home component to display wind status and humidity with dynamic units

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,54 @@
 import { getWeatherDetails } from "./actions/geocoding";
-import SearchBar from "./components/SearchBar";
+import ForecastCard from "./components/ForecastCard";
 import SideBar from "./components/SideBar";
 
 export default async function Home({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
-  const { lat, lon, location,state } = await searchParams
-  const weatherData = await getWeatherDetails({ latitude: lat?Number(lat):20, longitude: lon?Number(lon):20 })
-  return (
-    <>
-    <SideBar weatherData={weatherData} location={location || "unknown"}/>
-    </>
-  );
+    const { lat, lon, location, state,units } = await searchParams
+    const weatherData = await getWeatherDetails({ latitude: lat ? Number(lat) : 20, longitude: lon ? Number(lon) : 20 },units || 'metric')
+    const measureUnit = units || "metric"
+    const temperatureUnit = measureUnit === "metric" ? "°C" : measureUnit === "imperial" ? "°F" : "K";
+    const speedUnit = measureUnit === "metric" ? "km/h" : measureUnit === "imperial" ? "miles/hr" : "m/s";
+
+
+    return (
+        <>
+            <div className="flex">
+
+                <SideBar weatherData={weatherData} location={location || "unknown"} units={temperatureUnit} />
+
+                <div className="flex flex-wrap">
+                    {weatherData.daily.slice(1, 4).map(day => <ForecastCard key={day.dt} weatherData={day} units={temperatureUnit} />)}
+
+                </div>
+                <div>
+                    <div>
+                        <div className="card w-80">
+                            <div className="card-body">
+                                <h2 className="card-header">Wind status</h2>
+
+                                <p className='text-content2'>{`${weatherData.current.wind_speed} ${speedUnit}`}</p>
+                                <div className="card-footer">
+                                    <p className='text-content1'>{`${weatherData.current.wind_deg} °`}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card w-80">
+                            <div className="card-body">
+                                <h2 className="card-header">Humidity</h2>
+
+                                <p className='text-content2'>{`${weatherData.current.humidity} %`}</p>
+                                <div className="card-footer">
+                                    <progress className="progress" value={weatherData.current.humidity} max="100"></progress>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
