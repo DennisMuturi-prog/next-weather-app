8eafcba26f35f4e59d6a65401041f036bf628f4d
Dennis Muturi <deynohmuturi@gmail.com>
2025-05-02 21:06:19 +0300

    feat: add unit selection functionality to SearchBar component

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -3,58 +3,85 @@ import { ChangeEvent, useEffect, useState } from 'react'
 import { getLocationCoordinates, getUserLocationFromCoordinates } from '../actions/geocoding'
 import { GeoCoderLocation } from '../types'
 import { from, fromEvent } from 'rxjs'
-import { debounceTime,switchMap,distinctUntilChanged,map,filter ,tap,catchError} from 'rxjs/operators'
-import { usePathname, useRouter } from 'next/navigation'
+import { debounceTime, switchMap, distinctUntilChanged, map, filter, tap, catchError } from 'rxjs/operators'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 
 const SearchBar = () => {
     const [relevantLocations, setRelevantLocations] = useState<GeoCoderLocation[]>([])
-    const router=useRouter()
-    const pathName=usePathname()
+    const searchParams = useSearchParams()
+
+    const router = useRouter()
+    const pathName = usePathname()
     useEffect(() => {
-        if(pathName==="/"){
-            if('geolocation' in navigator){
-                navigator.geolocation.getCurrentPosition(async ({coords})=>{
-                    const {latitude,longitude}=coords
-                    const userLocation=await getUserLocationFromCoordinates({latitude,longitude})
-                    router.push(`/?location=${userLocation[0].name}&state=${userLocation[0].state}&lat=${latitude}&lon=${longitude}`)
+        if (pathName === "/") {
+            if ('geolocation' in navigator) {
+                navigator.geolocation.getCurrentPosition(async ({ coords }) => {
+                    const { latitude, longitude } = coords
+                    const userLocation = await getUserLocationFromCoordinates({ latitude, longitude })
+                    router.push(`/?location=${userLocation[0].name}&state=${userLocation[0].state}&lat=${latitude}&lon=${longitude}&units=${searchParams.get("units") || "metric"}`)
                 })
             }
         }
-        
+
         const searchInput = document.getElementById("search-input");
         if (!searchInput) {
             console.error("Element with ID 'search' not found.");
             return;
         }
-        const subscription=fromEvent(searchInput, 'input').pipe(
-            map(event=>(event.target as HTMLInputElement).value),
+        const subscription = fromEvent(searchInput, 'input').pipe(
+            map(event => (event.target as HTMLInputElement).value),
             filter(searchTerm => searchTerm.length > 2),
             debounceTime(500),
             distinctUntilChanged(),
-            switchMap(searchKey =>from(getLocationCoordinates(searchKey as string)).pipe(
+            switchMap(searchKey => from(getLocationCoordinates(searchKey as string)).pipe(
                 catchError((err) => {
                     return [];
-                  })
-                
-            ) ))
+                })
+
+            )))
             .subscribe(setRelevantLocations);
-        
-        return ()=>subscription.unsubscribe()
+
+        return () => subscription.unsubscribe()
 
 
 
     }, [])
+    const handleUnitsChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const newUnit = event.target.value; // Get the selected unit
+        const currentParams = new URLSearchParams(searchParams.toString()); // Clone current query parameters
+        currentParams.set("units", newUnit); // Update the "units" parameter
+        router.push(`${pathName}?${currentParams.toString()}`); // Push the updated query parameters
+    };
+    const onSearchClick=()=>{
+        const location=relevantLocations[0]
+        router.push(`/search?location=${location.name}&state=${location.state}&lat=${location.lat}&lon=${location.lon}&units=${searchParams.get("units") || "metric"}`)
+    }
     return (
-        <div className="navbar-center">
-            <div className="dropdown">
-                <input className="input" placeholder="enter location"  id='search-input' />
-                <div className="dropdown-menu">
-                    {relevantLocations.map(location => <a key={`${location.lat}${location.lon}`} href={`/search?location=${location.name}&state=${location.state}&lat=${location.lat}&lon=${location.lon}`} className="dropdown-item text-sm">{`${location.name} ${location.country} ${location.state}`}</a>)}
+        <>
+            <div className="navbar-center">
+                <div className="dropdown">
+                    <input className="input" placeholder="enter location" id='search-input' />
+                    <div className="dropdown-menu">
+                        {relevantLocations.map(location => <a key={`${location.lat}${location.lon}`} href={`/search?location=${location.name}&state=${location.state}&lat=${location.lat}&lon=${location.lon}&units=${searchParams.get("units") || "metric"}`} className="dropdown-item text-sm">{`${location.name} ${location.country} ${location.state}`}</a>)}
+                    </div>
+                </div>
+                <button className="btn btn-primary" onClick={onSearchClick}>search</button>
+            </div>
+            <div className="navbar-end">
+                <div className="tabs tabs-boxed">
+                    <input type="radio" name="units" id="unit1" className="tab-toggle" value="metric" defaultChecked
+                        onChange={handleUnitsChange} />
+                    <label htmlFor="unit1" className="tab navbar-item">Celcius</label>
+                    <input type="radio" name="units" id="unit2" className="tab-toggle" value="imperial"
+                        onChange={handleUnitsChange} />
+                    <label htmlFor="unit2" className="tab">Fahrenheit</label>
+                    <input type="radio" name="units" id="unit3" className="tab-toggle" value="standard"
+                        onChange={handleUnitsChange} />
+                    <label htmlFor="unit3" className="tab">Kelvin</label>
                 </div>
             </div>
-            <button className="btn btn-primary">search</button>
-        </div>
+        </>
     )
 }
 
