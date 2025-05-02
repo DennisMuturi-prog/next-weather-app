5f7bf9a3d61eed42810480f55edadd699c78944b
Dennis Muturi <deynohmuturi@gmail.com>
2025-05-02 19:54:23 +0300

    refactor: streamline Home component by removing unused code and updating weather data retrieval

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,13 @@
 import { getWeatherDetails } from "./actions/geocoding";
-import SearchBar from "./components/SearchBar"
+import SearchBar from "./components/SearchBar";
+import SideBar from "./components/SideBar";
 
 export default async function Home({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
-  const { latitude, longitude, location } = await searchParams
-  const weatherData = await getWeatherDetails({ latitude: Number(latitude), longitude: Number(longitude) })
-  // const latitude=searchParams?.lat || 20
-  // const longitude=searchParams?.lon || 20
-  // const locationName=searchParams?.name || "no current location"
-
+  const { lat, lon, location,state } = await searchParams
+  const weatherData = await getWeatherDetails({ latitude: lat?Number(lat):20, longitude: lon?Number(lon):20 })
   return (
     <>
-      <div className="navbar">
-        <SearchBar />
-        <div className="navbar-end">
-          <div className="tabs tabs-boxed">
-            <input type="radio" name="units" id="unit1" className="tab-toggle" defaultChecked />
-            <label htmlFor="unit1" className="tab navbar-item">Celcius</label>
-            <input type="radio" name="units" id="unit2" className="tab-toggle" />
-            <label htmlFor="unit2" className="tab">Fahrenheit</label>
-            <input type="radio" name="units" id="unit3" className="tab-toggle" />
-            <label htmlFor="unit3" className="tab">Kelvin</label>
-          </div>
-        </div>
-      </div>
-      
-      <div>
-      </div>
+    <SideBar weatherData={weatherData} location={location || "unknown"}/>
     </>
   );
 }
