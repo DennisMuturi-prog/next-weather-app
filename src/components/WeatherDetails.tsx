21fe1ecbea532b759e062b15c2efd70cf2a8f36a
Dennis Muturi <deynohmuturi@gmail.com>
2025-05-03 14:32:14 +0300

    feat: add WeatherDetails component to display weather information with dynamic units

---
+++ src/components/WeatherDetails.tsx
@@ -0,0 +1,43 @@
+import { WeatherData } from "../types"
+import SideBar from "./SideBar"
+import ForecastCard from "./ForecastCard"
+
+function WeatherDetails({weatherData,location,temperatureUnit,speedUnit}:{weatherData:WeatherData,location:string,temperatureUnit:string,speedUnit:string}) {
+  return <>
+  <div className="flex">
+
+      <SideBar weatherData={weatherData} location={location || "unknown"} units={temperatureUnit} />
+
+      <div>
+          <div className="flex flex-wrap gap-2">
+              {weatherData.daily.slice(1, 4).map(day => <ForecastCard key={day.dt} weatherData={day} units={temperatureUnit} />)}
+          </div>
+          <div className="flex gap-2 mt-2">
+              <div className="card w-80">
+                  <div className="card-body">
+                      <h2 className="card-header">Wind status</h2>
+                      <p className='text-content2'>{`${weatherData.current.wind_speed} ${speedUnit}`}</p>
+                      <div className="card-footer">
+                          <p className='text-content1'>{`${weatherData.current.wind_deg} °`}</p>
+                      </div>
+                  </div>
+              </div>
+              <div className="card w-80">
+                  <div className="card-body">
+                      <h2 className="card-header">Humidity</h2>
+                      <p className='text-content2'>{`${weatherData.current.humidity} %`}</p>
+                      <div className="card-footer">
+                          <progress className="progress" value={weatherData.current.humidity} max="100"></progress>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div>
+          </div>
+      </div>
+  </div>
+</>
+
+}
+
+export default WeatherDetails
