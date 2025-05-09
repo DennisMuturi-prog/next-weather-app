362c9a93692509619a3c8142d6c808994c94c6db
Dennis Muturi <deynohmuturi@gmail.com>
2025-05-09 23:49:49 +0300

    feat: enhance SearchBar component with geolocation support and improved navigation links

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { ChangeEvent, useEffect, useState } from 'react'
 import { getLocationCoordinates, getUserLocationFromCoordinates } from '../actions/geocoding'
-import { GeoCoderLocation } from '../types'
+import { Coordinates, GeoCoderLocation, UserLocation } from '../types'
 import { from, fromEvent } from 'rxjs'
 import { debounceTime, switchMap, distinctUntilChanged, map, filter, tap, catchError } from 'rxjs/operators'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
@@ -12,20 +12,25 @@ import Link from 'next/link'
 const SearchBar = () => {
     const [searchLocation, setSearchLocation] = useState("")
     const [relevantLocations, setRelevantLocations] = useState<GeoCoderLocation[]>([])
+    const [userCoordinates,setUserCoordinates]=useState<Coordinates>()
+    const [userLocation,setUserLocation]=useState<UserLocation>()
+
     const searchParams = useSearchParams()
 
     const router = useRouter()
     const pathName = usePathname()
     useEffect(() => {
-        if (pathName === "/") {
-            if ('geolocation' in navigator) {
-                navigator.geolocation.getCurrentPosition(async ({ coords }) => {
-                    const { latitude, longitude } = coords
-                    const userLocation = await getUserLocationFromCoordinates({ latitude, longitude })
-                    router.push(`/?location=${userLocation[0].name}&state=${userLocation[0].state}&lat=${latitude}&lon=${longitude}&units=${searchParams.get("units") || "metric"}`)
-                })
-            }
+        if ('geolocation' in navigator) {
+            console.log("I was run")
+            navigator.geolocation.getCurrentPosition(async ({ coords }) => {
+                setUserCoordinates(coords)
+                const { latitude, longitude } = coords
+                const userLocation = await getUserLocationFromCoordinates({ latitude, longitude })
+                setUserLocation(userLocation[0])
+                router.push(`/?location=${userLocation[0].name}&state=${userLocation[0].state}&lat=${latitude}&lon=${longitude}&units=${searchParams.get("units") || "metric"}`)
+            })
         }
+
 
         const searchInput = document.getElementById("search-input");
         if (!searchInput) {
@@ -72,13 +77,13 @@ const SearchBar = () => {
     return (
         <>
             <div className="navbar-start">
-                <a href={`/?units=${searchParams.get("units") || "metric"}`}>Home</a>
+                <Link href={`/?units=${searchParams.get("units") || "metric"}&lat=${userCoordinates?.latitude}&lon=${userCoordinates?.longitude}&location=${userLocation?.name}&state=${userLocation?.state}`}>Home</Link>
             </div>
             <div className="navbar-center">
                 <div className="dropdown">
                     <input className="input" placeholder="enter location" id='search-input' onChange={e => setSearchLocation(e.target.value)} />
                     <div className="dropdown-menu">
-                        {relevantLocations.map(location => <a key={`${location.lat}${location.lon}`} href={`/search?location=${location.name}&state=${location.state}&lat=${location.lat}&lon=${location.lon}&units=${searchParams.get("units") || "metric"}`} className="dropdown-item text-sm">{`${location.name} ${location.country} ${location.state}`}</a>)}
+                        {relevantLocations.map(location => <Link key={`${location.lat}${location.lon}`} href={`/?location=${location.name}&state=${location.state}&lat=${location.lat}&lon=${location.lon}&units=${searchParams.get("units") || "metric"}`} className="dropdown-item text-sm">{`${location.name} ${location.country} ${location.state}`}</Link>)}
                     </div>
                 </div>
                 <button className="btn btn-primary" onClick={onSearchClick}>search</button>
