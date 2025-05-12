eadf48926d46f66f1482a3d1f47b38f1b52c3a6e
Dennis Muturi <deynohmuturi@gmail.com>
2025-05-12 19:13:47 +0300

    feat: add state parameter to SideBar component for enhanced location display

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,7 +2,7 @@ import { formatDate } from "../actions/utilities"
 import { WeatherData } from "../types"
 import Image from "next/image"
 
-function SideBar({ weatherData,location,units }: { weatherData: WeatherData,location:string,units:string }) {
+function SideBar({ weatherData,location,state,units }: { weatherData: WeatherData,location:string,state:string,units:string }) {
     return (
         <aside className="sidebar justify-start">
             <section className="sidebar-content pl-4">
@@ -13,8 +13,7 @@ function SideBar({ weatherData,location,units }: { weatherData: WeatherData,loca
                     alt="weather condition" />
                 <p>{`${weatherData.current.temp} ${units}`}</p>
                 <p className="mt-10">{formatDate(weatherData.current.dt)}</p>
-                <p>{location}</p>
-
+                <p>{`${location} , ${state}`}</p>
             </section>
             <section className="sidebar-footer justify-end">
             </section>
