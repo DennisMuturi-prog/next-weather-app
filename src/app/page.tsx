d8b8ea9e88a3713d9cfb45f8f37f51d883887831
Dennis Muturi <deynohmuturi@gmail.com>
2025-05-12 19:13:23 +0300

    feat: include state parameter in weather details component for improved location context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
-import { getWeatherDetails } from "../actions/geocoding";
+import { getWeatherDetails } from "../actions/weatherdetailsfetch";
 import WeatherDetails from "../components/WeatherDetails";
 
 export default async function Home({ searchParams }: { searchParams:Promise< { [key: string]: string | undefined } >}) {
-    const { lat, lon, location,units } = await searchParams
+    const { lat, lon, location,units,state } = await searchParams
     const weatherData = await getWeatherDetails({ latitude: lat ? Number(lat) : 20, longitude: lon ? Number(lon) : 20 },units || 'metric')
     const measureUnit = units || "metric"
     const temperatureUnit = measureUnit === "metric" ? "°C" : measureUnit === "imperial" ? "°F" : "K";
     const speedUnit = measureUnit === "metric" ? "km/h" : measureUnit === "imperial" ? "miles/hr" : "m/s";
+    const stateParam=state || 'unknown'
 
 
     return (
-        <WeatherDetails weatherData={weatherData} location={location || "unkwown"} temperatureUnit={temperatureUnit} speedUnit={speedUnit}/>
+        <WeatherDetails weatherData={weatherData} location={location || "unkwown"} temperatureUnit={temperatureUnit} speedUnit={speedUnit} state={stateParam}/>
     );
 }
