4d080fc16d6d3ec8262f13c860d1ed958da63bfd
Dennis Muturi <deynohmuturi@gmail.com>
2025-05-12 19:35:58 +0300

    refactor: consolidate API_KEY and BACKEND_URL usage in getWeatherDetails function

--- src/actions/weatherdetailsfetch.ts
+++ src/actions/weatherdetailsfetch.ts
@@ -1,13 +1,29 @@
 import { Coordinates,WeatherData } from "@/types"
+
+const API_KEY =process.env.API_KEY
+const BACKEND_URL=process.env.BACKEND_URL
 export async function getWeatherDetails(locationCoordinates:Coordinates,units:string):Promise<WeatherData>{
-    const apiKey = process.env.API_KEY
     const {latitude,longitude}=locationCoordinates
-    const res=await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&units=${units}exclude={minutely,hourly,alerts}&appid=${apiKey}`,{
-        method:"GET",
-        // headers:{
-        //     "Content-Type":"application/json"
-        // }
-    })
-    const data=res.json()
-    return data
+    if(BACKEND_URL=='https://api.openweathermap.org/data/3.0/onecall'){
+        const res=await fetch(`${BACKEND_URL}?lat=${latitude}&lon=${longitude}&units=${units}exclude={minutely,hourly,alerts}&appid=${API_KEY}`,{
+            method:"GET",
+            // headers:{
+            //     "Content-Type":"application/json"
+            // }
+        })
+        const data=res.json()
+        return data
+    }
+    else{
+        const res=await fetch(`${BACKEND_URL}?lat=${latitude}&lon=${longitude}&units=${units}`,{
+            method:"GET",
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })
+        const data=res.json()
+        return data
+
+
+    }
 }
