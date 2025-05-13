07503bae419b61e3861ff3fb0d27fbce290c480f
Dennis Muturi <deynohmuturi@gmail.com>
2025-05-13 21:58:45 +0300

    fix: correct exclude parameter formatting in weather API request

--- src/actions/weatherdetailsfetch.ts
+++ src/actions/weatherdetailsfetch.ts
@@ -5,7 +5,7 @@ const BACKEND_URL=process.env.BACKEND_URL
 export async function getWeatherDetails(locationCoordinates:Coordinates,units:string):Promise<WeatherData>{
     const {latitude,longitude}=locationCoordinates
     if(BACKEND_URL=='https://api.openweathermap.org/data/3.0/onecall'){
-        const res=await fetch(`${BACKEND_URL}?lat=${latitude}&lon=${longitude}&units=${units}&exclude={minutely,hourly,alerts}&appid=${API_KEY}`,{
+        const res=await fetch(`${BACKEND_URL}?lat=${latitude}&lon=${longitude}&units=${units}&exclude=minutely,hourly,alerts&appid=${API_KEY}`,{
             method:"GET",
             // headers:{
             //     "Content-Type":"application/json"
