d4a9292eb95ba692dffa2a3cbffd1fdd1fdf0490
Dennis Muturi <deynohmuturi@gmail.com>
2025-05-14 19:28:12 +0300

    fix: update default latitude and longitude values, and set default state to 'Dubai'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import WeatherDetails from "../components/WeatherDetails";
 
 export default async function Home({ searchParams }: { searchParams:Promise< { [key: string]: string | undefined } >}) {
     const { lat, lon, location,units,state } = await searchParams
-    const weatherData = await getWeatherDetails({ latitude: lat ? Number(lat) : 20, longitude: lon ? Number(lon) : 20 },units || 'metric')
+    const weatherData = await getWeatherDetails({ latitude: lat ? Number(lat) : 25.26534, longitude: lon ? Number(lon) : 55.2924914 },units || 'metric')
     const measureUnit = units || "metric"
     const temperatureUnit = measureUnit === "metric" ? "°C" : measureUnit === "imperial" ? "°F" : "K";
     const speedUnit = measureUnit === "metric" ? "km/h" : measureUnit === "imperial" ? "miles/hr" : "m/s";
-    const stateParam=state || 'unknown'
+    const stateParam=state || 'Dubai'
 
 
     return (
-        <WeatherDetails weatherData={weatherData} location={location || "unkwown"} temperatureUnit={temperatureUnit} speedUnit={speedUnit} state={stateParam}/>
+        <WeatherDetails weatherData={weatherData} location={location || "Dubai"} temperatureUnit={temperatureUnit} speedUnit={speedUnit} state={stateParam}/>
     );
 }
