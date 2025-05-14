42847ac737c4be806b7391eac4a2b7edb09631c7
Dennis Muturi <deynohmuturi@gmail.com>
2025-05-14 19:28:30 +0300

    feat: implement location permission handling and refactor geolocation fetching in SearchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -22,21 +22,6 @@ const SearchBar = () => {
     const router = useRouter()
     const pathName = usePathname()
     useEffect(() => {
-        if ('geolocation' in navigator) {
-            navigator.geolocation.getCurrentPosition(async ({ coords }) => {
-                setUserCoordinates(coords)
-                const { latitude, longitude } = coords
-                const userLocation = await getUserLocationFromCoordinates({ latitude, longitude })
-                setUserLocation(userLocation[0])
-                if (!searchParams.get("location")) {
-                    startTransition(() => {
-                        router.push(`/?location=${userLocation[0].name}&state=${userLocation[0].state}&lat=${latitude}&lon=${longitude}&units=${searchParams.get("units") || "metric"}`)
-                    })
-                }
-            })
-        }
-
-
         const searchInput = document.getElementById("search-input");
         if (!searchInput) {
             console.error("Element with ID 'search' not found.");
@@ -68,6 +53,57 @@ const SearchBar = () => {
 
 
     }, [])
+    const askForLocationPermission = () => {
+        navigator.permissions.query({ name: "geolocation" }).then(async (result) => {
+            if (result.state === "granted") {
+                // Permission granted
+                await fetchUserGeoLocation()
+            } else if (result.state === "prompt") {
+                // Permission not yet granted or denied
+                toast(
+                    (t) => (
+                        <span>We need your permission for location so as to know the weather near you
+                            <div>
+                                <button className='btn btn-primary' onClick={async () => {
+                                    toast.dismiss(t.id)
+                                    await fetchUserGeoLocation()
+                                }
+                                }>Accept</button>
+                                <button className='btn btn-error' onClick={() => toast.dismiss(t.id)}>Deny</button>
+                            </div>
+                        </span>
+                    ),
+                    {
+                        duration: 5000,
+                    }
+                );
+
+            } else if (result.state === "denied") {
+                toast.error("you denied access to location",{
+                    duration:1000
+                })
+                // Permission denied
+            }
+        });
+
+
+
+
+    }
+    const fetchUserGeoLocation = async () => {
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(async ({ coords }) => {
+                setUserCoordinates(coords)
+                const { latitude, longitude } = coords
+                const userLocation = await getUserLocationFromCoordinates({ latitude, longitude })
+                setUserLocation(userLocation[0])
+                startTransition(() => {
+                    router.push(`/?location=${userLocation[0].name}&state=${userLocation[0].state}&lat=${latitude}&lon=${longitude}&units=${searchParams.get("units") || "metric"}`)
+                })
+            })
+        }
+
+    }
     const handleUnitsChange = (event: ChangeEvent<HTMLInputElement>) => {
         const newUnit = event.target.value; // Get the selected unit
         const currentParams = new URLSearchParams(searchParams.toString()); // Clone current query parameters
@@ -85,19 +121,25 @@ const SearchBar = () => {
                 loading: 'fetching coordinates',
                 success: (data) => {
                     const firstLocale = data[0]
+                    setDropDownOpen(false)
                     startTransition(() => {
                         router.push(`?lat=${firstLocale.lat}&lon=${firstLocale.lon}&location=${firstLocale.name}&state=${firstLocale.state}&units=${searchParams.get('units') || 'metric'}`)
                     })
                     return data.length > 0 ? "successfully fetched coordinates" : "no results found"
                 },
                 error: 'an error occurred while fetching coordinates'
+            },
+            {
+                position: "bottom-center"
             }
         )
     }
     return (
         <>
             <div className="navbar-start">
-                <Link href={`/?units=${searchParams.get("units") || "metric"}&lat=${userCoordinates?.latitude}&lon=${userCoordinates?.longitude}&location=${userLocation?.name}&state=${userLocation?.state}`}>Home</Link>
+                <button className="btn btn-outline-primary" onClick={() => askForLocationPermission()
+                }>My Location</button>
+
             </div>
             <div className="navbar-center">
                 <div className="dropdown">
@@ -109,12 +151,12 @@ const SearchBar = () => {
                                 startTransition(() => {
                                     router.push(`/?location=${location.name}&state=${location.state ? location.state : "unkown"}&lat=${location.lat}&lon=${location.lon}&units=${searchParams.get("units") || "metric"}`)
                                 })
-                            }}>{`${location.name} ${location.country} ${location.state}`}</button>) : isPending?(<a className='dropdown-item'>
+                            }}>{`${location.name} ${location.country} ${location.state}`}</button>) : isPending ? (<a className='dropdown-item'>
                                 <div className="spinner-simple spinner-sm"></div>
-                            </a>):(<a className='dropdown-item text-sm'>no results</a>)}
+                            </a>) : (<a className='dropdown-item text-sm'>no results</a>)}
                     </div>}
                 </div>
-                {(isPending&&!dropdownOpen) && <div className="spinner-simple"></div>}
+                {(isPending && !dropdownOpen) && <div className="spinner-simple"></div>}
                 <button className="btn btn-primary" onClick={onSearchClick}>search</button>
             </div>
             <div className="navbar-end">
